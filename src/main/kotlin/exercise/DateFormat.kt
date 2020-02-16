168572090282e7dc981e74fb5799321e194e36bb
satokodai <kodaisato551@gmail.com>
2020-02-16 22:02:20 +0900

    inicial

---
+++ src/main/kotlin/exercise/DateFormat.kt
@@ -0,0 +1,17 @@
+package ru.yole.jkid
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class DateFormat(val format: String)
+
+class DateSerializer(format: String) : ValueSerializer<Date> {
+    private val simpleDateFormat = SimpleDateFormat(format)
+
+    override fun toJsonValue(value: Date): Any? =
+            simpleDateFormat.format(value)
+
+    override fun fromJsonValue(jsonValue: Any?): Date =
+            simpleDateFormat.parse(jsonValue as String)
+}
